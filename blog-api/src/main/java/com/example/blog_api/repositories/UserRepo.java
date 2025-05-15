0498387661dfcfb20ab4bac5bbfd37cd92b9697e
RohitPatil <rohit.patil@atyeti.com>
2025-05-15 10:21:59 +0530

    project added

--- blog-api/src/main/java/com/example/blog_api/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.blog_api.repositories;
-
-import com.example.blog_api.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-
-}
