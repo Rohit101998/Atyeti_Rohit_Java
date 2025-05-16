fb5212b9d7fb56ccdc1886ea4a5cb65f73934c0b
Rohit101998 <rohit.patil@atyeti.com>
2025-05-16 10:03:18 +0530

    Delete JSON_Operation directory

--- JSON_Operation/src/main/java/org/example/Exception/DuplicateEmployeeException.java
+++
@@ -1,7 +0,0 @@
-package org.example.Exception;
-
-public class DuplicateEmployeeException extends RuntimeException{
-    public DuplicateEmployeeException(String message){
-        super(message);
-    }
-}
