81a958852cc3b79e122227926e7ac0bab5bb95bf
RohitPatil <rohit.patil@atyeti.com>
2025-05-16 10:12:37 +0530

    project added

--- blog-api/src/main/java/com/example/blog_api/servises/Impl/UserServiceImpl.java
+++ blog-api/src/main/java/com/example/blog_api/servises/Impl/UserServiceImpl.java
@@ -43,7 +43,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto getUserById(Integer userId) {
         User user = this.userRepo.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
+                .orElseThrow(() -> new ResourceNotFoundException("User", " Id ", userId));
         return this.userToDto(user);
 
     }
