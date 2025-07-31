cec587a893102d9cf1aaae98d27aa777f343bcb4
RohitPatil <rohit.patil@atyeti.com>
2025-07-31 09:16:43 +0530

    sb-ecom Project

--- sb-ecom/sb-ecom/src/main/java/com/ecommarce/project/service/CartServiceImpl.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommarce/project/service/CartServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
@@ -102,12 +103,19 @@ public class CartServiceImpl implements CartService{
         List<CartDTO> cartDTOs = carts.stream()
                 .map(cart -> {
                     CartDTO cartDTO = modelMapper.map(cart,CartDTO.class);
-                    List<ProductDTO> products = cart.getCartItems().stream()
-                            .map(p-> modelMapper.map(p.getProduct(),ProductDTO.class))
-                            .toList();
+
+//                    List<ProductDTO> products = cart.getCartItems().stream()
+//                            .map(p-> modelMapper.map(p.getProduct(),ProductDTO.class))
+//                            .toList();
+
+        List<ProductDTO> products = cart.getCartItems().stream().map(cartItem -> {
+            ProductDTO productDTO = modelMapper.map(cartItem.getProduct(),ProductDTO.class);
+            productDTO.setQuantity(cartItem.getQuantity());
+            return productDTO;
+        }).collect(Collectors.toList());
                     cartDTO.setProducts(products);
                     return cartDTO;
-                }).toList();
+                }).collect(Collectors.toList());
         return cartDTOs;
     }
 
